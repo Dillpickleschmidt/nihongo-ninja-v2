dc6cbb7ade3deeee0dc7708a066b891d74230e29
Dylan Kuzmick <drkuzmick@gmail.com>
2024-06-09 13:49:00 -0400

    re-add greetings-quiz json
    fix: picture-quiz json location

--- src/app/learn/(lessons)/chapter-0/greetings-quiz/page.tsx
+++ src/app/learn/(lessons)/chapter-0/greetings-quiz/page.tsx
@@ -13,7 +13,7 @@ export default function page() {
       // backgroundImageSize="1215px"
       // backgroundImageOpacity={5}
     >
-      {/* <PictureQuizWrapper /> */}
+      <PictureQuizWrapper />
       <div></div>
     </ContentBox>
   )

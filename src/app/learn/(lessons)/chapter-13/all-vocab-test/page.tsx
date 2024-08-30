3f99bad01172024f8e5f4235d8bd5ea6cdc40571
Dillpickleschmidt <drkuzmick@gmail.com>
2024-08-30 17:47:37 -0400

    fix: missing async

--- src/app/learn/(lessons)/chapter-13/all-vocab-test/page.tsx
+++ src/app/learn/(lessons)/chapter-13/all-vocab-test/page.tsx
@@ -1,8 +1,13 @@
 import { fetchVocabularyByPath } from "@/features/practice-mode/components/fetchVocabByPath"
 import VocabTest from "@/features/vocab-test/VocabTest"
 
-const data = await fetchVocabularyByPath("chapter-13/nouns")
+export default async function page() {
+  const nouns = await fetchVocabularyByPath("chapter-13/nouns")
+  const adj_and_verbs = await fetchVocabularyByPath("chapter-13/adj-and-verbs")
+  const day_count_and_misc = await fetchVocabularyByPath(
+    "chapter-13/day-count-and-misc",
+  )
+  const data = [...nouns, ...adj_and_verbs, ...day_count_and_misc]
 
-export default function page() {
   return <VocabTest data={data} chapter={13} />
 }
